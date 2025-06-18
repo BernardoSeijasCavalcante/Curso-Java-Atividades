0f7b6c2ab40628e99c82dc9339108d4e8b170d5c
BernardoSeijasCavalcante <bernardomseijas@gmail.com>
2025-06-18 13:57:39 -0300

    Comments of UserResource

--- webServiceJPAHibernate/src/main/java/br/bernardoSeijasCavalcante/webServiceJPAHibernate/resources/UserResource.java
+++ webServiceJPAHibernate/src/main/java/br/bernardoSeijasCavalcante/webServiceJPAHibernate/resources/UserResource.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import br.bernardoSeijasCavalcante.webServiceJPAHibernate.entities.User;
 
 @RestController //Falar que essa classe é um recurso Web implementado por um controlador rest
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users")//Indica que todos os métodos no interior desta classe responderão a url /users
 public class UserResource {
-	@GetMapping
+	@GetMapping//Indica que o método abaixo responde a requisições GET em /users
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria", "maria", "maria");
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(u);//Retorna um HTTP 200 ok com um objeto java u em um corpo JSON.
 	}
+	
+	/*ResponseEntity é o encapsulamento de uma resposta HTTP,
+	 * ela encapsula o corpo da resposta, o status da resposta,
+	 * os cabeçalhos e etc.
+	 */
 }
